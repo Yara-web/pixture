31adb42970affd908eda4cddd49918857849c032
Yaroslav <69323968+eReader1@users.noreply.github.com>
2021-04-16 00:34:45 +0300

    asd

--- js/main.js
+++ js/main.js
@@ -2,4 +2,6 @@ document.querySelector('.burger-menu').addEventListener('click', function(){
 	document.querySelector('.burger__nav').classList.toggle('active');
 	document.querySelector('.burger-menu').classList.toggle('active');
 	document.querySelector('body').classList.toggle('lock');
-})
+});
+
+
